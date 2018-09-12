103595677dadc786663b4b7e592efcaa08d1fb6f
WHUERXuJiajun <43143690+WHUERXuJiajun@users.noreply.github.com>
2018-09-12 18:06:47 +0800

    Add files via upload

---
+++ Homework1/ConsoleSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("请输入两个数字：");
+            Console.Write("a=");
+            var a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("a*b=" + a * b);
+
+
+            
+        }
+    }
+}
