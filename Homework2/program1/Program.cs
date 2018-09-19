83db2d21e6d7194091d1b7cd69ca7b89f108ec4e
WHUERXuJiajun <43143690+WHUERXuJiajun@users.noreply.github.com>
2018-09-19 18:07:11 +0800

    Add files via upload

---
+++ Homework2/program1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数据(0---(2^31-1)) ：");
+            int num;
+            bool TF = int.TryParse(Console.ReadLine(), out num);
+            if (TF){
+                Console.WriteLine('\n');
+                getPrimeList(num);
+                Console.WriteLine('\n');
+            }
+            else{
+                Console.WriteLine("请输入 0 到 （2^31-1）内的数字");
+            }
+        }
+        static void getPrimeList(int num)
+        {
+            int  NPrime = 0;
+            for (int i = 2; i <= (num / 2); i++){
+                if (num % i == 0){
+                    Console.Write(i + " ");
+                    NPrime = num / i;
+                    break;
+                }
+            }
+            if (NPrime != 0){
+                getPrimeList(NPrime);
+            }
+            else{
+                Console.Write(num);
+            }
+        }
+    }
+}
---
+++ Homework2/program2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Max = 10;
+            int[] a1 = new int[Max];
+            int sum = 0;
+            Console.WriteLine("请输入十个整数");
+            for (int i = 0; i < Max; i++){
+                string temp = Console.ReadLine();
+                a1[i] = Int32.Parse(temp);
+                sum += a1[i];
+            }
+            int average = sum / Max;
+            int[] a2 = a1;
+            int max = 0;
+            for (int i = 0; i < Max - 1; i++){
+                if (a2[i] > a2[i + 1]){
+                    max = a2[i];
+                }
+                else{
+                    max = a2[i + 1];
+                }
+            }
+            Console.WriteLine("最大值:" + max);
+
+            int[] a3 = a1;
+            int min = 0;
+            for (int i = 0; i < Max - 1; i++){
+                if (a3[i] < a3[i + 1]) { 
+                
+                    min = a3[i];
+                }
+                else{
+                
+                    min = a3[i + 1];
+                }
+            }
+            Console.WriteLine("最小值:" + min);
+            Console.WriteLine("平均值:" + average);
+            Console.WriteLine("和:" + sum);
+        }
+    }
+}
